a81a0fcadd6e2deaa733184f5a78fa9a64ef127d
kmcmanus <mcmanus.keith@gmail.com>
2018-09-26 18:03:06 +0100

    Changed program to read in a name from the user and use this in output.

--- HelloWorldGit/Program.cs
+++ HelloWorldGit/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,9 @@ namespace HelloWorldGit
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World");
+            Console.Write("Please enter your name >> ");
+            string name = Console.ReadLine();
+            Console.Write($"Hello {name}");
             Console.ReadLine();
         }
     }
